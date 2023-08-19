8309132591c68b084acd9809496d09eeb3519357
GabrielCosme <cosmebarbosa.gabriel@gmail.com>
2023-08-19 05:05:33 -0300

    🎨 Update utils functions to float

--- inc/utils.hpp
+++ inc/utils.hpp
@@ -1,9 +1,12 @@
 #ifndef __UTILS_HPP__
 #define __UTILS_HPP__
 
-#define constrain(value, min, max) (value < min ? min : (value > max ? max : value))
+constexpr float constrain(float value, float min, float max) {
+    return value < min ? min : (value > max ? max : value);
+}
 
-#define map(value, from_min, from_max, to_min, to_max) \
-    (to_min + (to_max - to_min) * (value - from_min) / (from_max - from_min))
+constexpr float map(float value, float from_min, float from_max, float to_min, float to_max) {
+    return to_min + (to_max - to_min) * (value - from_min) / (from_max - from_min);
+}
 
 #endif // __UTILS_HPP__
